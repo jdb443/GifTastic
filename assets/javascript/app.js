a2b80b93c36b3b1ffed363dd5939d1937b65ce58
Jonathan Behar <jdb443@stern.nyu.edu>
2019-05-24 16:25:52 -0400

    sports

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,5 @@
 // Jonathan Behar Homework 6 GifTastic
 
+var myData = ["Baseball", "Soccer", "Swimming", "Running", "Biking", "Tennis", "Hiking", 
+"Cheerleading", "Basketball", "Football", "Hockey", "Polo", "Waterpolo", "Tug-O-War", "Fencing", 
+"Lightsaber Fencing", "Boxing", "Surfing", "Sailing", "MMA", "Volleyball" ];
